744c47aac9bcb4848430e00e728077aab72188b7
Donatas Noreika <donoras@gmail.com>
2023-08-29 08:58:57 +0300

    Modelių stulpeliai atvaizduojami knygos listview

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -32,6 +32,11 @@ class Book(models.Model):
     isbn = models.CharField(verbose_name="ISBN", max_length=13, help_text='13 Simbolių <a href="https://www.isbn-international.org/content/what-isbn">ISBN kodas</a>')
     genre = models.ManyToManyField(to="Genre", help_text='Išrinkite žanrą(us) šiai knygai')
 
+    def display_genre(self):
+        genres = self.genre.all()
+        res = ", ".join(genre.name for genre in genres)
+        return res
+
     def __str__(self):
         return self.title
 

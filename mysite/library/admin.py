2036f17936b396ba494d148ab3e6e156daa9e9ed
Donatas Noreika <donoras@gmail.com>
2023-08-29 09:27:51 +0300

    Nustatytas BookInstance atvaizdavimas admin puslapyje (stulpeliai, filtras, laukų išdėstymas)

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -4,8 +4,18 @@ from .models import Author, Book, Genre, BookInstance
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'isbn', 'display_genre']
 
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['book', 'uuid', 'due_back']
+    list_filter = ['book', 'status', 'due_back']
+
+    fieldsets = (
+        ('General', {'fields': ('uuid', 'book')}),
+        ('Availability', {'fields': ('status', 'due_back')}),
+    )
+
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Book, BookAdmin)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)

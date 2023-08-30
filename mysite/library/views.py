177f9581fc5a0b744f922b221fc767a06abcf024
Donatas Noreika <donoras@gmail.com>
2023-08-30 09:41:16 +0300

    Atnaujintas index.html su statistine bibliotekos informacija

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, BookInstance, Author
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', context={})
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    num_instances_available = BookInstance.objects.filter(status__exact="g").count()
+    num_authors = Author.objects.count()
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+    return render(request, 'index.html', context=context)

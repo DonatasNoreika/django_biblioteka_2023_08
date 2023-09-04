571344991029cdf61d2b712fa912de1fc3662068
Donatas Noreika <donoras@gmail.com>
2023-09-04 10:03:34 +0300

    Įdėta knygų paieška pagal pavadinimą, aprašymą, autoriaus vardą ir pavardę

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Book, BookInstance, Author
 from django.views import generic
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -48,3 +49,15 @@ class BookDetailView(generic.DetailView):
     model = Book
     template_name = "book.html"
     context_object_name = "book"
+
+
+def search(request):
+    """
+    paprasta paieška. query ima informaciją iš paieškos laukelio,
+    search_results prafiltruoja pagal įvestą tekstą knygų pavadinimus ir aprašymus.
+    Icontains nuo contains skiriasi tuo, kad icontains ignoruoja ar raidės
+    didžiosios/mažosios.
+    """
+    query = request.GET.get('query')
+    search_results = Book.objects.filter(Q(title__icontains=query) | Q(summary__icontains=query) | Q(author__last_name__icontains=query) | Q(author__first_name__icontains=query))
+    return render(request, 'search.html', {'books': search_results, 'query': query})

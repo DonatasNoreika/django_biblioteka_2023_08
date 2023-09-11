aa5cd71b7b7eec3fdffd45b9bc5b7d0ddca541df
Donatas Noreika <donoras@gmail.com>
2023-09-11 10:22:45 +0300

    10 prisegta profilio nuotrauka automatiškai sumažinama

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -3,7 +3,7 @@ import uuid
 from django.contrib.auth.models import User
 from datetime import date
 from tinymce.models import HTMLField
-
+from PIL import Image
 
 # Create your models here.
 class Genre(models.Model):
@@ -101,3 +101,11 @@ class Profilis(models.Model):
 
     def __str__(self):
         return f"{self.user.username} profilis"
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        super().save(force_insert, force_update, using, update_fields)
+        img = Image.open(self.nuotrauka.path)
+        if img.height > 300 or img.width > 300:
+            img.thumbnail((300, 300))
+            img.save(self.nuotrauka.path)
+

820217bec46e53d9d71d9ba7a56740ad8851721d
Donatas Noreika <donoras@gmail.com>
2023-09-13 10:35:40 +0300

    12: Išverstas knygos modelis

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from datetime import date
 from tinymce.models import HTMLField
 from PIL import Image
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Genre(models.Model):
@@ -37,25 +38,27 @@ class Author(models.Model):
 
 
 class Book(models.Model):
-    title = models.CharField(verbose_name="Pavadinimas", max_length=200)
-    author = models.ForeignKey(to="Author", on_delete=models.SET_NULL, null=True, related_name="books")
-    summary = models.TextField(verbose_name="Aprašymas", max_length=1000, help_text='Trumpas knygos aprašymas')
+    title = models.CharField(verbose_name=_("Title"), max_length=200)
+    author = models.ForeignKey(to="Author", verbose_name=_("Author"), on_delete=models.SET_NULL, null=True, related_name="books")
+    summary = models.TextField(verbose_name=_("Description"), max_length=1000, help_text=_('Shot book summary'))
     isbn = models.CharField(verbose_name="ISBN", max_length=13,
                             help_text='13 Simbolių <a href="https://www.isbn-international.org/content/what-isbn">ISBN kodas</a>')
-    genre = models.ManyToManyField(to="Genre", help_text='Išrinkite žanrą(us) šiai knygai')
-    cover = models.ImageField(verbose_name="Viršelis", upload_to='covers', null=True, blank=True)
+    genre = models.ManyToManyField(to="Genre", verbose_name=_("Genres"), help_text='Išrinkite žanrą(us) šiai knygai')
+    cover = models.ImageField(verbose_name=_("Cover"), upload_to='covers', null=True, blank=True)
 
     def display_genre(self):
         genres = self.genre.all()
         res = ", ".join(genre.name for genre in genres)
         return res
 
+    display_genre.short_description = _("Display Genre")
+
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Knyga'
-        verbose_name_plural = 'Knygos'
+        verbose_name = _("Book")
+        verbose_name_plural = _("Books")
 
 
 class BookReview(models.Model):
